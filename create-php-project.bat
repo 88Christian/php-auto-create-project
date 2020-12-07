1719e4627a36c6c33149c07eb5db819e6953fb24
Christian <88Christian>
2020-12-07 14:36:47 -0800

    updated php auto create

--- create-php-project.bat
+++ create-php-project.bat
@@ -6,6 +6,11 @@ md %name%
 cd %name%
 md img css js font
 echo >> index.php
+echo >> signup.php
+echo >> login.php
+echo >> connection.php
+echo >> functions.php
+echo >> logout.php
 cd css
 echo >> index.css
 cd..
